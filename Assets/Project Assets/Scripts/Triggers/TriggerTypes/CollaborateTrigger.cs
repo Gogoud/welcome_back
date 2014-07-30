65bc61d57f7d4e6946f3ad20ed6ff72e54bb840f
Johan <Johan.Ahlgren@hotmail.com>
2014-07-30 11:25:32 +0200

    Merge johan

---
+++ Assets/Project Assets/Scripts/Triggers/TriggerTypes/CollaborateTrigger.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+/* Discription: ObjectComponent class for interactions between two specific objects
+ * 
+ * Created by: Robert Datum: 08/04/14
+ * Modified by: Jimmy Datum 14/04/14
+ * Modified by: Sebastian Datum 23/04/14: Changed the raycast to cast from the camera, and changed GetButtonDown to GetButtonUp
+ * Modified by: Jon Wahlstrom 2014-04-29 "added functionallity for collaborate hover effect"
+ * Modified by: Rasmus 07/07/14 Haven't done anything just felt left out :P
+ */
+[RequireComponent(typeof(PickUp))]
+public class CollaborateTrigger : ObjectComponent
+{
+	#region PublicMemberVariables
+	private List<int>	m_ValidId = new List<int>();
+	private string		m_Input;
+	#endregion
+
+	void Start()
+	{
+		SuperTrigger[] triggerArray;
+		triggerArray = gameObject.GetComponents<SuperTrigger>();
+		foreach(SuperTrigger c in triggerArray)
+		{
+			if(c.m_CollaborateSelf){
+				m_ValidId = c.m_IDsCollaborate;
+				m_Input = c.m_CollaborateInput;
+			}
+		}
+	}
+
+	public override void Interact ()
+	{
+		RaycastHit hit;
+		Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+		Debug.DrawRay (ray.origin, ray.direction * Camera.main.gameObject.GetComponent<Raycasting>().m_Distance, Color.green);
+
+		if(Input.GetButtonDown(m_Input))
+		{
+			// change m_Distance
+			if(Physics.Raycast (ray, out hit, Camera.main.gameObject.GetComponent<Raycasting>().m_Distance, Camera.main.gameObject.GetComponent<Raycasting>().m_LayerMask.value))
+			{
+				ObjectComponent hoover = hit.collider.gameObject.GetComponent<ObjectComponent>();
+				if(hoover.gameObject.GetComponent<Id>() != null)
+				{
+					int collisionId = hoover.gameObject.GetComponent<Id>().ObjectId;
+					if(m_ValidId.Contains(collisionId) && hoover != null)
+					{
+						Camera.main.GetComponent<Raycasting>().ShowCollaborateHover = true;
+					
+						if(hoover.gameObject.GetComponent<SuperTrigger>())
+						{
+							SuperTrigger[] triggerArray;
+							triggerArray = hoover.gameObject.GetComponents<SuperTrigger>();
+							foreach(SuperTrigger c in triggerArray)
+							{
+								if(c.m_CollaborateGet){
+									c.ActivateTrigger();
+								}
+							}
+						}
+						if(gameObject.GetComponent<SuperTrigger>())
+						{
+							SuperTrigger[] triggerArray;
+							triggerArray = gameObject.GetComponents<SuperTrigger>();
+							foreach(SuperTrigger c in triggerArray)
+							{
+								if(c.m_CollaborateSelf){
+									c.ActivateTrigger();
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			// change m_Distance
+			if(Physics.Raycast (ray, out hit, Camera.main.gameObject.GetComponent<Raycasting>().m_Distance, Camera.main.gameObject.GetComponent<Raycasting>().m_LayerMask.value))
+			{
+				ObjectComponent hoover = hit.collider.gameObject.GetComponent<ObjectComponent>();
+				if(hoover.gameObject.GetComponent<Id>() != null)
+				{
+					int collisionId = hoover.gameObject.GetComponent<Id>().ObjectId;
+				
+					if(m_ValidId.Contains(collisionId) && hoover != null)
+					{
+						Camera.main.GetComponent<Raycasting>().ShowCollaborateHover = true;
+					}
+				}
+				else
+				{
+					Camera.main.GetComponent<Raycasting>().ShowCollaborateHover = false;
+				}
+			}
+			else
+			{
+				Camera.main.GetComponent<Raycasting>().ShowCollaborateHover = false;
+			}
+		}
+
+	}
+}
---
+++ Assets/Project Assets/Scripts/Triggers/TriggerTypes/OnClickTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickTrigger : MonoBehaviour 
+{
+	public void OnMouseDown()
+	{
+		if(gameObject.GetComponent<SuperTrigger>())
+		{
+			SuperTrigger[] triggerArray;
+			triggerArray = gameObject.GetComponents<SuperTrigger>();
+			foreach(SuperTrigger c in triggerArray)
+			{
+				if(c.m_OnClick){
+					c.ActivateTrigger();
+				}
+			}
+		}
+	}
+}

d8e21e3f91c0fa7417b01aba9798e40a893475a8
Gogoud <abotroll_kleffe@hotmail.se>
2014-08-10 17:06:47 +0200

    big ass update 2

---
+++ Assets/CameraCulling.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () 
+{
+	var distances = new float[32];
+	distances[8] = 9;
+	distances[9] = 9;
+	camera.layerCullDistances = distances;
+	camera.layerCullSpherical = true;
+
+}
+
+function Update () {
+
+}

a2553fa7185efae0f2159de94618d9272c0f6ae9
Gogoud <abotroll_kleffe@hotmail.se>
2014-09-17 18:14:34 +0200

    baseball scare

---
+++ Assets/ActivateGravity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody.useGravity = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}

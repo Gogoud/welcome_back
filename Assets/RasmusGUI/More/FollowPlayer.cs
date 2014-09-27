ff512cfe446b1d776c619848d1a1531cc414ad3d
viktor <abotroll_kleffe@hotmail.se>
2014-09-27 16:48:47 +0200

    småfix

--- Assets/RasmusGUI/More/FollowPlayer.cs
+++ Assets/RasmusGUI/More/FollowPlayer.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class FollowPlayer : MonoBehaviour 
 {
-	public NavMeshAgent m_Agent;
+	//public NavMeshAgent m_Agent;
 	public GameObject m_Player;
 	// Use this for initialization
 	void Start () 
 	{
-		m_Agent = GetComponent<NavMeshAgent>();
+		m_Player = GameObject.FindGameObjectWithTag ("Player");
+		//m_Agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		m_Agent.SetDestination (m_Player.transform.position);
+		transform.LookAt(m_Player.transform);
+		//m_Agent.SetDestination (m_Player.transform.position);
 	}
 }
